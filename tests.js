e70a65558b4a32a82d32f1e9b6c4d90c18781178
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-13 15:55:25 -0500

    pt 3 sayHello

--- tests.js
+++ tests.js
@@ -17,10 +17,16 @@ describe('helloWorld', function() {
 //Unit test for sayHello function
 describe( 'sayHello',function() {
     it('should be a defined function', function() {
-        expect(typeof sayHello).toBe("function");
+        expect(typeof sayHello).toBe('function');
     });
     it('should return a string when called', function() {
        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+       expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
 
 });
